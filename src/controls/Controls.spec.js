02292c6f5ba79e2aa1d824a75ba6b54e27d00525
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-14 14:05:31 +0100

    set up afterEAch, wrapper and beforeEach in controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,4 +2,16 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
 
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach( () => {
+    wrapper = rtl.render (<Controls/>);
+})
+
+describe('Control component', () => {
+    
+})
