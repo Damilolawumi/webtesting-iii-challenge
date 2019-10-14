068a22ece7e6ff6aff811b0fed60bc40e29f6447
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-14 14:05:57 +0100

    imported the libraries needed in dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach ( () => {
+    wrapper = rtl.render(<Dashboard/>);
+})
+
+describe('Dashboard component', () => {
+    
+})
